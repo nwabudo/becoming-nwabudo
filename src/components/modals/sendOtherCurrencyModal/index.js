c01359ab1b5a443584a5d027323a7a1bbac075d8
Abayomi Olatunji <abayomijohn273@gmail.com>
2024-05-13 13:41:12 +0100

    fix: update ui

---
+++ src/components/modals/sendOtherCurrencyModal/index.js
@@ -0,0 +1,96 @@
+import { Dialog, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { Fragment, useRef } from "react";
+import NairaImg from "../../../../public/images/webp/send_naira.webp";
+import BankLogoImage from "../../../../public/images/webp/wema.webp";
+
+const AccountCard = ({ name, value }) => {
+  return <div className="text-lg md:text-xl lg:text-xl xl:text-2xl flex justify-between text-white">
+    <p>{name}</p>
+    <p className="font-bold text-right">{value}</p>
+  </div>
+}
+
+const SendOtherCurrencyModal = ({
+  open,
+  handleClose,
+}) => {
+  const cancelButtonRef = useRef(null);
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-20"
+        initialFocus={cancelButtonRef}
+        onClose={handleClose}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-80 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-20 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative w-full transform overflow-hidden bg-primary text-left transition-all sm:my-8 sm:w-full md:max-w-xl">
+                <div className={`relative p-4 md:p-6 xl:p-10 h-[8.75rem] md:h-[10.75rem]`}>
+                  <div className="flex justify-between items-center gap-8 relative z-10">
+                    <h2 className='text-white font-bold text-4xl xl:text-5xl font-rusilla'>
+                      Send usd, gbp
+                    </h2>
+                    <button onClick={handleClose}>
+                      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M25.7075 24.2926C25.8004 24.3855 25.8741 24.4958 25.9244 24.6172C25.9747 24.7386 26.0006 24.8687 26.0006 25.0001C26.0006 25.1315 25.9747 25.2616 25.9244 25.383C25.8741 25.5044 25.8004 25.6147 25.7075 25.7076C25.6146 25.8005 25.5043 25.8742 25.3829 25.9245C25.2615 25.9747 25.1314 26.0006 25 26.0006C24.8686 26.0006 24.7385 25.9747 24.6171 25.9245C24.4957 25.8742 24.3854 25.8005 24.2925 25.7076L16 17.4138L7.70751 25.7076C7.51987 25.8952 7.26537 26.0006 7.00001 26.0006C6.73464 26.0006 6.48015 25.8952 6.29251 25.7076C6.10487 25.5199 5.99945 25.2654 5.99945 25.0001C5.99945 24.7347 6.10487 24.4802 6.29251 24.2926L14.5863 16.0001L6.29251 7.70757C6.10487 7.51993 5.99945 7.26543 5.99945 7.00007C5.99945 6.7347 6.10487 6.48021 6.29251 6.29257C6.48015 6.10493 6.73464 5.99951 7.00001 5.99951C7.26537 5.99951 7.51987 6.10493 7.70751 6.29257L16 14.5863L24.2925 6.29257C24.4801 6.10493 24.7346 5.99951 25 5.99951C25.2654 5.99951 25.5199 6.10493 25.7075 6.29257C25.8951 6.48021 26.0006 6.7347 26.0006 7.00007C26.0006 7.26543 25.8951 7.51993 25.7075 7.70757L17.4138 16.0001L25.7075 24.2926Z" fill="white" />
+                      </svg>
+                    </button>
+                  </div>
+
+
+                  <div className="w-full h-full absolute top-0 left-0">
+                    <Image src={NairaImg} className="w-full h-full object-cover" alt="" />
+                  </div>
+                </div>
+                <div className="mx-auto w-[7.625rem] h-[7rem] bg-white -translate-y-12 flex items-center justify-center px-3 py-4">
+                  <Image src={BankLogoImage} className="" alt="" />
+                </div>
+                <div className="bg-primary pb-16 px-[2rem] md:px-[4rem]">
+                  <div className="w-full md:w-11/12 mx-auto flex flex-col space-y-2">
+                    <AccountCard
+                      name={"Account Number:"}
+                      value={"0237408235"}
+                    />
+                    <AccountCard
+                      name={"Bank:"}
+                      value={"Wema Bank"}
+                    />
+                    <AccountCard
+                      name={"Account Name:"}
+                      value={"Chioma Ozoigbo"}
+                    />
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+
+export default SendOtherCurrencyModal
---
+++ src/modules/home/middleSection/components/support.js
@@ -0,0 +1,92 @@
+"use client"
+import SendNairaModal from "@/components/modals/sendNairaModal"
+import SendOtherCurrencyModal from "@/components/modals/sendOtherCurrencyModal"
+import Image from "next/image"
+import { useState } from "react"
+import OtherCurrencyImg from "../../../../../public/images/webp/currency.webp"
+import NairaImg from "../../../../../public/images/webp/naira.webp"
+
+const PaymentCard = ({ title, image, handleSelectPaymentOption }) => {
+  return <button
+    type="button"
+    onClick={handleSelectPaymentOption}
+    className="relative focus:outline-none overflow-hidden shadow-[0px_4px_30px_0px_rgba(0,0,0,0.1)] bg-white group"
+  >
+    <div className="h-[13rem] md:h-[14rem] lg:h-[16rem] xl:h-[20.438rem] w-full relative overflow-hidden bg-textColor">
+      <Image src={image || NairaImg} alt="" className="h-full w-full object-cover transition-all ease-out duration-200 group-hover:scale-[1.05] group-hover:origin-center group-hover:grayscale" />
+    </div>
+    <div className="py-10 px-8 md:px-10 flex items-center justify-between gap-4">
+      <p className="text-primary text-lg md:text-xl lg:text-2xl xl:text-[1.75rem] font-bold 
+      group-hover:text-textColor transition-all ease-in-out duration-200">
+        {title}
+      </p>
+      <div className="group-hover:translate-x-1 transition-all ease-in-out duration-200">
+        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clip-path="url(#clip0_202_486)">
+            <path d="M16 3C13.4288 3 10.9154 3.76244 8.77759 5.1909C6.63975 6.61935 4.97351 8.64968 3.98957 11.0251C3.00563 13.4006 2.74819 16.0144 3.2498 18.5362C3.75141 21.0579 4.98953 23.3743 6.80762 25.1924C8.6257 27.0105 10.9421 28.2486 13.4638 28.7502C15.9856 29.2518 18.5995 28.9944 20.9749 28.0104C23.3503 27.0265 25.3807 25.3603 26.8091 23.2224C28.2376 21.0846 29 18.5712 29 16C28.9964 12.5533 27.6256 9.24882 25.1884 6.81163C22.7512 4.37445 19.4467 3.00364 16 3ZM21.7075 16.7075L17.7075 20.7075C17.5199 20.8951 17.2654 21.0006 17 21.0006C16.7346 21.0006 16.4801 20.8951 16.2925 20.7075C16.1049 20.5199 15.9994 20.2654 15.9994 20C15.9994 19.7346 16.1049 19.4801 16.2925 19.2925L18.5863 17H11C10.7348 17 10.4804 16.8946 10.2929 16.7071C10.1054 16.5196 10 16.2652 10 16C10 15.7348 10.1054 15.4804 10.2929 15.2929C10.4804 15.1054 10.7348 15 11 15H18.5863L16.2925 12.7075C16.1049 12.5199 15.9994 12.2654 15.9994 12C15.9994 11.7346 16.1049 11.4801 16.2925 11.2925C16.4801 11.1049 16.7346 10.9994 17 10.9994C17.2654 10.9994 17.5199 11.1049 17.7075 11.2925L21.7075 15.2925C21.8005 15.3854 21.8742 15.4957 21.9246 15.6171C21.9749 15.7385 22.0008 15.8686 22.0008 16C22.0008 16.1314 21.9749 16.2615 21.9246 16.3829C21.8742 16.5043 21.8005 16.6146 21.7075 16.7075Z" fill="#D8B067" />
+          </g>
+          <defs>
+            <clipPath id="clip0_202_486">
+              <rect width="32" height="32" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  </button>
+}
+
+const Support = () => {
+  const [showNairaModal, setShowNairaModal] = useState(false)
+  const [showOtherCurrencyModal, setShowOtherCurrencyModal] = useState(false)
+
+  const handleSelectPaymentOption = (val) => {
+    if (val === "naira") {
+      setShowNairaModal(true)
+    } else {
+      setShowOtherCurrencyModal(true)
+    }
+  }
+
+  const handleClose = () => {
+    setShowNairaModal(false)
+    setShowOtherCurrencyModal(false)
+  }
+
+  return (
+    <>
+      <div className="mt-8 md:mt-12 lg:mt-16 xl:mt-20">
+        <h4 className="text-center font-rusilla text-white text-[3.25rem] leading-[0.8] md:text-[5.25rem] md:leading-[0.7] xl:text-[6.25rem] xl:leading-[0.7]">
+          support
+          <br />
+          our journey
+        </h4>
+
+        <div className="mt-14 md:mt-20 mx-auto w-full md:w-11/12 lg:w-10/12 grid grid-cols-1 md:grid-cols-2 gap-5">
+          <PaymentCard
+            image={NairaImg}
+            title={"Send Naira"}
+            handleSelectPaymentOption={() => handleSelectPaymentOption("naira")}
+          />
+          <PaymentCard
+            image={OtherCurrencyImg}
+            title={"Send other currency"}
+            handleSelectPaymentOption={() => handleSelectPaymentOption("others")}
+          />
+        </div>
+      </div>
+
+      <SendNairaModal
+        open={showNairaModal}
+        handleClose={handleClose}
+      />
+
+      <SendOtherCurrencyModal
+        open={showOtherCurrencyModal}
+        handleClose={handleClose}
+      />
+    </>
+  )
+}
+
+export default Support
